7cf25ed862ae3e384fb7f53cf6757b52f3374c04
Darshana Priyasad <darshanapriyasad.13@cse.mrt.ac.lk>
2015-12-16 08:17:56 -0800

    Third Commit

--- app/src/main/java/com/example/darshanapriyasad/taxi_service/MainWindow.java
+++ app/src/main/java/com/example/darshanapriyasad/taxi_service/MainWindow.java
@@ -1,11 +1,13 @@
 package com.example.darshanapriyasad.taxi_service;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class MainWindow extends AppCompatActivity {
 
@@ -16,14 +18,15 @@ public class MainWindow extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+        Button button = (Button) findViewById(R.id.driverButton);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(MainWindow.this,HireTaxi.class);
+                startActivity(intent);
             }
         });
+
     }
 
 }
