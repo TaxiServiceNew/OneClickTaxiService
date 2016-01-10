af8f9d2882d6ea596f53e949b6c3ffb3a6f60698
Buddhi Ayesha <buddhiayesha.13@cse.mrt.ac.lk>
2016-01-10 06:06:46 +0530

    Set the Button Seventeenth Comit

--- app/src/main/java/com/example/darshanapriyasad/taxi_service/CustomerReservation.java
+++ app/src/main/java/com/example/darshanapriyasad/taxi_service/CustomerReservation.java
@@ -1,20 +1,65 @@
 package com.example.darshanapriyasad.taxi_service;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.github.jjobes.slidedatetimepicker.SlideDateTimeListener;
+import com.github.jjobes.slidedatetimepicker.SlideDateTimePicker;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class CustomerReservation extends AppCompatActivity {
 
+    private SimpleDateFormat mFormatter = new SimpleDateFormat("MMMM dd yyyy hh:mm aa");
+
+    private SlideDateTimeListener listener = new SlideDateTimeListener() {
+
+        @Override
+        public void onDateTimeSet(Date date)
+        {
+            Toast.makeText(CustomerReservation.this,
+                    mFormatter.format(date), Toast.LENGTH_SHORT).show();
+        }
+
+        // Optional cancel listener
+        @Override
+        public void onDateTimeCancel()
+        {
+            Toast.makeText(CustomerReservation.this,
+                    "Canceled", Toast.LENGTH_SHORT).show();
+        }
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer_reservation);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        Button button = (Button) findViewById(R.id.buttonDateTime);
+
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v)
+            {
+                new SlideDateTimePicker.Builder(getSupportFragmentManager())
+                        .setListener(listener)
+                        .setInitialDate(new Date())
+                                //.setMinDate(minDate)
+                                //.setMaxDate(maxDate)
+                                //.setIs24HourTime(true)
+                                //.setTheme(SlideDateTimePicker.HOLO_DARK)
+                                //.setIndicatorColor(Color.parseColor("#990000"))
+                        .build()
+                        .show();
+            }
+        });
 
 
     }
