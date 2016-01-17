d107802dc0accee64faabf026214e8cc6a979ec4
Darshana Priyasad <darshanapriyasad.13@cse.mrt.ac.lk>
2016-01-16 23:13:11 -0800

    Nineteenth Commit Feedback Done

---
+++ app/src/main/java/com/example/darshanapriyasad/taxi_service/CustomerFeedBack.java
@@ -0,0 +1,13 @@
+package com.example.darshanapriyasad.taxi_service;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CustomerFeedBack extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer_feed_back);
+    }
+}
---
+++ app/src/main/java/com/example/darshanapriyasad/taxi_service/CustomerFeedBackForm.java
@@ -0,0 +1,22 @@
+package com.example.darshanapriyasad.taxi_service;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class CustomerFeedBackForm extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer_feed_back_form);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+    }
+
+}
